91e4e1087bf3b8e135e116d7af3ae0e5b62c7f69
lessjiu <97648384+lessjiu@users.noreply.github.com>
2022-02-04 13:22:42 -0800

    fixed

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void subtractTest() {
-        assertEquals(5, SkillDemo.subtract(10, 1)); //should fail
+        assertEquals(5, SkillDemo.subtract(10, 5));
     }
 }
